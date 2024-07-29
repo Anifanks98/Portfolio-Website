a6c364c7f6e8f563113d944ed5ecb1392eeb0b41
Anirudhan C <anirudhanc.ani@gmail.com>
2024-07-29 10:50:29 +0530

    added few animations

--- script.js
+++ script.js
@@ -5,3 +5,45 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+gsap.from(".section-container", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".section-container",
+    scroller: "body",
+    start: "top 70%",
+    markers: true,
+    end: "top 60%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".about-details-container", {
+  y: 50,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".about-details-container",
+    scroller: "body",
+    start: "top 70%",
+    markers: true,
+    end: "top 60%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".experience-details-container", {
+  y: 50,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".experience-details-container",
+    scroller: "body",
+    start: "top 70%",
+    markers: true,
+    end: "top 60%",
+    scrub: 2,
+  },
+});
